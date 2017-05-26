2e9ff231910dda766ceb4e2dacb89fac8e1c975b
MathieuB8 <masamune31@hotmail.com>
2017-05-26 21:22:10 +0200

    adding script

--- script.py
+++ script.py
@@ -1,1 +1,19 @@
+interestingindex =[]
+f = open('example.csv', 'r')
+res = open('res.csv','w')
+line = f.readline()
+print "res:" +line
+line = line.split(";")
+interestingindex.append(line.index("data"));
+interestingindex.append(line.index("country"));
+#print str(interestingindex);
+print "====\n";
+for lines in f:
+	#print lines,
+	lines = lines.split(";")
+	for a in interestingindex:
+		print str(lines[a])+";",
+	print "\n"
+print "====\n";
+
 
