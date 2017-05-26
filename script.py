d98184c464e9a3da37370577af5901e9240b62fb
MathieuB8 <masamune31@hotmail.com>
2017-05-26 21:40:59 +0200

    updates

--- script.py
+++ script.py
@@ -2,17 +2,47 @@ interestingindex =[]
 f = open('example.csv', 'r')
 res = open('res.csv','w')
 line = f.readline()
+
+tmp=line.split(";")
 print "res:" +line
 line = line.split(";")
 interestingindex.append(line.index("data"));
 interestingindex.append(line.index("country"));
-#print str(interestingindex);
+
+#First line case, the header
+for a in interestingindex:
+	if tmp[a] == 'data':
+		print "OMGOMG"
+		if a != interestingindex[-1]:
+			result="datav2"+";"
+		else:
+			result="datav2"
+	elif tmp[a] == 'country':
+		if a != interestingindex[-1]:
+			result="countryv2"+";"
+		else:
+			result="countryv2"
+	else:
+		if a != interestingindex[-1]:
+			result=tmp[a]+";"
+		else:
+			result=tmp[a]
+	res.write(result)
+res.write("\n")
 print "====\n";
+
+#The other lines, the data, not the header
 for lines in f:
 	#print lines,
 	lines = lines.split(";")
 	for a in interestingindex:
+		if a != interestingindex[-1]:
+			temp = lines[a]+";"
+		else:
+			temp = lines[a]
+		res.write(temp)
 		print str(lines[a])+";",
+	res.write("\n");
 	print "\n"
 print "====\n";
 
